b1e04d8e2838d768a80f4650131d1186cbacc160
OKatayeva <oksana89@gmail.com>
2021-09-06 16:10:32 +0300

    identity setup

--- AShop/Startup.cs
+++ AShop/Startup.cs
@@ -64,6 +64,7 @@ namespace AShop
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
