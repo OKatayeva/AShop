b9c5c5191b0b49281dcd5102debf36a8ab7d6dec
OKatayeva <oksana89@gmail.com>
2021-09-06 16:11:33 +0300

    identity files added

---
+++ AShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,29 @@
+using System;
+using AShop.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(AShop.Areas.Identity.IdentityHostingStartup))]
+namespace AShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AshopDB>(options =>
+                    options.UseSqlServer(
+                        //context.Configuration.GetConnectionString("AShopIdentityDbContextConnection")));
+                        context.Configuration.GetConnectionString("DefaultConnection")));
+
+
+                //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                  //  .AddEntityFrameworkStores<AshopDB>();
+            });
+        }
+    }
+}
