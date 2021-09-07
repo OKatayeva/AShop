2f8081faadf3a85733d496d15b35a90d579152ba
OKatayeva <oksana89@gmail.com>
2021-09-07 22:41:36 +0300

    CartController added, ProductUserViewModel added.

--- AShop/Controllers/CartController.cs
+++ AShop/Controllers/CartController.cs
@@ -1,19 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using AShop.Data;
 using AShop.Models;
+using AShop.Models.ViewModels;
 using AShop.Utility;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace AShop.Controllers
 {
+    [Authorize]
     public class CartController : Controller
     {
         private readonly AshopDB _context;
+
+        [BindProperty]
+        public ProductUserViewModel ProductUserViewModel { get; set; }
+
         public CartController(AshopDB context)
         {
             _context = context;
@@ -32,5 +40,51 @@ namespace AShop.Controllers
             return View(productList);
         }
 
+        public IActionResult Remove(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            shoppingCartList.Remove(shoppingCartList.FirstOrDefault(u => u.ProductId == id));
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [ActionName ("Index")]
+        public IActionResult IndexPost()
+        {
+            
+            return RedirectToAction(nameof(Summary));
+        }
+
+        public IActionResult Summary()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //var userId = User.FindFirstValue(ClaimTypes.Name);
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //session exists
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> productList = _context.Product.Where(u => prodInCart.Contains(u.Id));
+            ProductUserViewModel = new ProductUserViewModel()
+            {
+                ApplicationUser = _context.ApplicationUser.FirstOrDefault(u => u.Id == claim.Value)
+            };
+
+            return View(ProductUserViewModel);
+        }
     }
 }
---
+++ AShop/Models/ViewModels/ProductUserViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace AShop.Models.ViewModels
+{
+    public class ProductUserViewModel
+    {
+        public ProductUserViewModel()
+        {
+            ProductList = new List<Product>();
+        }
+
+
+        public ApplicationUser ApplicationUser { get; set; }
+
+        public IEnumerable<Product> ProductList { get; set; }
+    }
+}
