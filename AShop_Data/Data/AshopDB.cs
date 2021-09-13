8fcb1c40b30d529702243fa60e36296902e7f23a
OKatayeva <oksana89@gmail.com>
2021-09-14 02:07:38 +0300

    Data project file

---
+++ AShop_Data/Data/AshopDB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using AShop_Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AShop_Data
+{
+    public class AshopDB : IdentityDbContext
+    {
+        public AshopDB (DbContextOptions<AshopDB> options): base(options)
+        {
+
+        }
+
+        public DbSet <Category> Category { get; set; }
+
+        public DbSet<ApplicationType> ApplicationType { get; set; }
+
+        public DbSet<Product> Product { get; set; }
+
+        public DbSet<ApplicationUser> ApplicationUser { get; set; }
+    }
+}
