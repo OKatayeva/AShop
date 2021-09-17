faf1b0c245cb4371a57b7f2c0862ca245790f5b1
OKatayeva <oksana89@gmail.com>
2021-09-17 18:58:12 +0300

    Repository added

---
+++ AShop_Data/Repository/Repository.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Linq;
+using AShop_Data.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace AShop_Data.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AshopDB _context;
+        internal DbSet<T> dbSet;
+
+        public Repository(AshopDB context)
+        {
+            _context = context;
+            this.dbSet = _context.Set<T>();
+        }
+
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Find(int id)
+        {
+            return dbSet.Find(id);
+        }
+
+        public T FirstOrDefault(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if (includeProperties != null)
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+
+                }
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return query.FirstOrDefault();
+        }
+
+        public System.Collections.Generic.IEnumerable<T> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> filter = null, Func<System.Linq.IQueryable<T>, System.Linq.IOrderedQueryable<T>> orderBy = null, string includeProperties = null, bool isTracking = true)
+        {
+            IQueryable<T> query = dbSet;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            if(includeProperties != null)
+            {
+                foreach(var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+
+                }
+            }
+            if(orderBy != null)
+            {
+                query = orderBy(query);
+            }
+            if (!isTracking)
+            {
+                query = query.AsNoTracking();
+            }
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
