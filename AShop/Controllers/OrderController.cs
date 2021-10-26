626cfdd360cb46d3a52138e5f49fab5369eb5651
OKatayeva <oksana89@gmail.com>
2021-10-27 02:19:08 +0300

    Order Index created

---
+++ AShop/Controllers/OrderController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AShop_Data.Repository.IRepository;
+using AShop_Models.ViewModels;
+using AShop_Utility;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AShop.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderHeaderRepository _orderHeaderRepo;
+        private readonly IOrderDetailRepository _orderDetailRepo;
+
+        public OrderController(IOrderHeaderRepository orderHeaderRepo,
+            IOrderDetailRepository orderDetailRepo)
+        {
+            _orderHeaderRepo = orderHeaderRepo;
+            _orderDetailRepo = orderDetailRepo;
+
+        }
+
+        public IActionResult Index()
+        {
+            OrderListVM orderListVM = new OrderListVM()
+            {
+                OrderHeaderList = _orderHeaderRepo.GetAll(),
+                StatusList = WC.listStatus.ToList().Select(i => new Microsoft.AspNetCore.Mvc.Rendering.SelectListItem
+                {
+                    Text = i,
+                    Value = i
+                })
+            };
+            
+            return View(orderListVM);
+        }
+    }
+}
---
+++ AShop_Models/ViewModels/OrderListVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace AShop_Models.ViewModels
+{
+    public class OrderListVM
+    {
+        public IEnumerable<OrderHeader> OrderHeaderList { get; set; }
+
+        public IEnumerable<SelectListItem> StatusList { get; set; }
+
+        public string Status { get; set; }
+    }
+}
